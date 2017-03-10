dff3e15fd39dcb9717968d46cb5fe86fd89fa754
MattyWang <chenmatty.wang@hikster.com>
2017-03-10 10:22:17 -0500

    added clock

--- matty-to-do/src/App.js
+++ matty-to-do/src/App.js
@@ -5,9 +5,10 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {items: []};
+    this.state = {items: [], clock: ""};
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.tick = this.tick.bind(this);
   }
 
   handleSubmit(e) {
@@ -15,13 +16,22 @@ class App extends Component {
     this.setState({items: this.state.items.concat(this.input.value)});
   }
 
-  render() {
-    console.log(this.state)
+  tick() {
+    this.setState({
+      clock: new Date().toLocaleTimeString()
+    })
+  }
 
+  componentDidMount() {
+    setInterval(this.tick, 1000)
+  }
+
+  render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Matty&apos;s To-Do List</h2>
+          <h2>{this.state.clock}</h2>
         </div>
 
         <form onSubmit={this.handleSubmit}>
@@ -33,7 +43,10 @@ class App extends Component {
         </form>
 
         <ul className="list">
-          {this.state.items.map( (item) => {return <li>{item}</li>}) }
+          {this.state.items.map( (item) => {return
+
+              <li>{item}</li>
+            })}
         </ul>
 
       </div>
